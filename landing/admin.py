c1250521d33d79c1c969ca7cb4ce0e1a8a0741bb
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-06 13:35:46 +0300

    уменьшен размер полей

--- landing/admin.py
+++ landing/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.forms import TextInput, Textarea
+
 from .models import *
 
 
@@ -6,21 +8,37 @@ from .models import *
 class HeadersAdmin(admin.ModelAdmin):
     list_display = ['title', 'is_published']
     list_editable = ['is_published']
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': 50})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 5, 'cols': 80})}
+    }
 
 
 @admin.register(Inspires)
 class InspiresAdmin(admin.ModelAdmin):
     list_display = ['title', 'is_published']
     list_editable = ['is_published']
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': 50})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 5, 'cols': 80})}
+    }
 
 
 @admin.register(Lets)
 class LetsAdmin(admin.ModelAdmin):
     list_display = ['title', 'is_published']
     list_editable = ['is_published']
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': 50})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 5, 'cols': 80})}
+    }
 
 
 @admin.register(Crm)
 class CrmAdmin(admin.ModelAdmin):
     list_display = ['title', 'is_published']
     list_editable = ['is_published']
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': 50})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 5, 'cols': 80})}
+    }
