ae0c8962e38821a138df51835f8c92c1fee41aed
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-06 14:37:06 +0300

    Зарегистрированна модель Галереи добавлена группировка полей для раздела Галереи

--- landing/admin.py
+++ landing/admin.py
@@ -42,3 +42,34 @@ class CrmAdmin(admin.ModelAdmin):
         models.CharField: {'widget': TextInput(attrs={'size': 50})},
         models.TextField: {'widget': Textarea(attrs={'rows': 5, 'cols': 80})}
     }
+
+
+@admin.register(Galleries)
+class GalleriesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'is_published']
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': 50})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 5, 'cols': 80})}
+    }
+    fieldsets = (
+        ('Галерея - Текстовая часть', {
+            'fields': ('title', 'content')
+        }),
+        ('Изображение для раздела Django', {
+            'fields': ('img_1_dj', 'img_2_dj', 'img_3_dj', 'img_4_dj', 'img_5_dj', 'img_6_dj', 'img_7_dj',
+                       'img_8_dj', 'img_9_dj')
+        }),
+        ('Изображение для раздела Bootstrap', {
+            'fields': ('img_1_boots', 'img_2_boots', 'img_3_boots', 'img_4_boots', 'img_5_boots', 'img_6_boots',
+                       'img_7_boots', 'img_8_boots', 'img_9_boots')
+        }),
+        ('Изображение для раздела CSS', {
+            'fields': ('img_1_css', 'img_2_css', 'img_3_css', 'img_4_css', 'img_5_css', 'img_6_css', 'img_7_css',
+                       'img_8_css', 'img_9_css')
+        }),
+        ('Опубликовать Галерею', {
+            'fields': ('is_published',),
+            'description': 'Прежде чем опубликовать второй вариант галереи - убедитесь что у предыдущего варианта '
+                           'галереии снято отметка -> ОПУБЛИКОВАНО'
+        }),
+    )
