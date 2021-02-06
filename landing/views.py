411c487670b5c1e6637cb29267986a14cef6cd62
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-06 22:26:17 +0300

    добавлен вывод только первых(сортировка в обратном порядке) трёх отзывов

--- landing/views.py
+++ landing/views.py
@@ -16,5 +16,7 @@ class Index(ListView):
         context['crm'] = Crm.objects.filter(is_published=True)
         context['gallery'] = Galleries.objects.filter(is_published=True)
         context['reviews_header'] = ReviewsHeader.objects.filter(is_published=True)
-        context['reviews'] = Reviews.objects.filter(is_published=True)[:3]
+        context['reviews1'] = Reviews.objects.filter(is_published=True)[:1]
+        context['reviews2'] = Reviews.objects.filter(is_published=True)[1:2]
+        context['reviews3'] = Reviews.objects.filter(is_published=True)[2:3]
         return context
