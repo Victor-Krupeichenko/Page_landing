005845e385728a950e9cbe4584ac90658c1e8cd7
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-10 16:39:36 +0300

    добавлен скрипт для кнопки вверх

--- landing/static/js/main.js
+++ landing/static/js/main.js
@@ -3,4 +3,16 @@ $(function () {
     animation: 'fadeIn',
     noScrollbars: true
 });
+	 $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.arrow_page_up').fadeIn();
+        } else {
+            $('.arrow_page_up').fadeOut();
+        }
+    });
+
+    $('.arrow_page_up').click(function () {
+        $('html, body').animate({scrollTop: 0}, 800);
+        return false;
+    });
 })
