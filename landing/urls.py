9ebd15ef5585aae7ae1bc3217999a27c4f481a8f
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-12 01:51:12 +0300

    маршрут для просмотра всех отзывов

--- landing/urls.py
+++ landing/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
-    path('reviews-add/', ReviewsCreate.as_view(), name='reviews_add')
+    path('reviews-add/', ReviewsCreate.as_view(), name='reviews_add'),
+    path('reviews-all/', ReviewsAll.as_view(), name='reviews_all')
 ]
