89d223a6497a602cb80390ac6f9f41735fb1b879
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-11 17:59:36 +0300

    дополнительная логика получения оформления сраниы добовления отзыва

--- landing/utils.py
+++ landing/utils.py
@@ -1,8 +1,16 @@
 from .forms import FormQuestionsViews
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+from .models import ReviewsAddText
 
 class MyFormLanding(CreateView):
     form_class = FormQuestionsViews
     template_name = 'index.html'
     success_url = reverse_lazy('home')
+
+class MyFormReviewsAdd(ListView):
+    model = ReviewsAddText
+    template_name = 'reviews_add.html'
+
+    def get_queryset(self):
+        return ReviewsAddText.objects.filter(is_published=True)
