3fee7d0f9d29139f8b6a2ed6c3206c9591a19e69
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-14 19:38:10 +0300

    Модели для блога

--- blog/models.py
+++ blog/models.py
@@ -1,26 +1,28 @@
 from django.db import models
+from django.urls import reverse_lazy
 
 
-class Blogs(models.Model):
-    title = models.CharField(max_length=50, verbose_name='Заголовок статьи')
-    slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name='URl-slug')
+class Notes(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    slug = models.SlugField(max_length=150, unique=True, db_index=True, verbose_name='URLs')
     content = models.TextField(verbose_name='Текст статьи')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редакирования')
-    photo = models.ImageField(upload_to='photo/blog/%m/%d/', blank=True, verbose_name='Фото')
-    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
-    category = models.ForeignKey('Categories', on_delete=models.PROTECT, verbose_name='Категория')
-
-    class Meta:
-        ordering =['-created_at']
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    image = models.ImageField(upload_to='image/notes/%m/%d/', verbose_name='Изображение', blank=True)
+    views = models.IntegerField(default=0, verbose_name='Просмотры')
+    is_published = models.BooleanField(default=False)
+    category = models.ForeignKey('BlogCategories', on_delete=models.PROTECT, verbose_name='Категория')
 
     def __str__(self):
         return self.title
 
 
-class Categories(models.Model):
-    title = models.CharField(max_length=20, verbose_name='Название категории')
-    slug = models.SlugField(max_length=20, unique=True, db_index=True, verbose_name='URl-slug')
+class BlogCategories(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name='URLs')
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse_lazy('category', kwargs={'slug': self.slug})
