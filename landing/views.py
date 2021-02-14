017bc93d1401038d3c8d6c341960a3181cf54b90
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-14 19:21:12 +0300

    переопределение тайтла для сайта

--- landing/views.py
+++ landing/views.py
@@ -14,6 +14,7 @@ class Index(MyFormLanding, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(Index, self).get_context_data(**kwargs)
+        context['title'] = 'Мой сайт'
         context['inspires'] = Inspires.objects.filter(is_published=True)
         context['lets'] = Lets.objects.filter(is_published=True)
         context['crm'] = Crm.objects.filter(is_published=True)
