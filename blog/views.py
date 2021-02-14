a8e5f35da3fdfb3e309ccafa9154bbcd944e08f8
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-14 19:42:27 +0300

    Контроллер-класс(NotesViews) для рендоринга основного шаблона блога. Контроллер-класс(NotesByCategories) для получения записи по категории, скрывать категории у которых нет записей

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.views.generic import ListView
+from .models import Notes, BlogCategories
+
+
+class NotesViews(ListView):
+    model = Notes
+    template_name = 'blog.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(NotesViews, self).get_context_data(**kwargs)
+        context['title'] = 'My Blog'
+        return context
+
+
+class NotesByCategories(ListView):
+    template_name = 'blog.html'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Notes.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(NotesByCategories, self).get_context_data(**kwargs)
+        context['title'] = BlogCategories.objects.get(slug=self.kwargs['slug'])
+        return context
