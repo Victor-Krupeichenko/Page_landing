5759faf6ea88b81b648162e4dd8a1b1f3f75d661
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-15 21:36:02 +0300

    Добавлен маршрут для просмотра конкретной записи

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import NotesViews, NotesByCategories
+from .views import NotesViews, NotesByCategories, ViewsNotes
 
-urlpatterns =[
+urlpatterns = [
     path('', NotesViews.as_view(), name='blog'),
-    path('category/<str:slug>/', NotesByCategories.as_view(), name='category' )
+    path('category/<str:slug>/', NotesByCategories.as_view(), name='category'),
+    path('vievs-notes/<str:slug>/', ViewsNotes.as_view(), name='views_notes'),
 ]
