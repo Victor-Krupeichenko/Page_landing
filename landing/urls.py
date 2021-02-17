476d78269108e677e43c3cf4c3a52ef6a3536b72
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-17 23:53:40 +0300

    Маршрут для просмотра конкретной записи из секции inspire

--- landing/urls.py
+++ landing/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
+    path('read-more-single/<str:slug>/', InspireViews.as_view(), name='single_inspire'),
     path('reviews-add/', ReviewsCreate.as_view(), name='reviews_add'),
     path('reviews-all/', ReviewsAll.as_view(), name='reviews_all')
 ]
