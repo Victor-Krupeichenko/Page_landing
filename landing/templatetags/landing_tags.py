f2438fa473cd984637e1ac1abd6b90f5fea44df3
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-19 23:10:38 +0300

    Пользовательские теги секции gallery - для получение заголовоков и изображений разделов галлереи

--- landing/templatetags/landing_tags.py
+++ landing/templatetags/landing_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from landing.models import InspireHeader, Inspires, Crm, CrmContent
+from landing.models import InspireHeader, Inspires, Crm, CrmContent, GalleriesTitles, GalleryImages
 
 register = template.Library()
 
@@ -17,22 +17,41 @@ def show_content():
     return {'contents': contents,
             'contents_1': contents_1}
 
+
 @register.inclusion_tag('_inc/inspire_design.html')
 def show_design():
     design = Inspires.objects.values('attainment_title', 'attainment_content').filter(is_published=True)
-    return {'design':design}
+    return {'design': design}
+
 
 @register.inclusion_tag('_inc/crm_title.html')
 def show_title_crm():
     titles = Crm.objects.filter(is_published=True)
-    return {'titles':titles}
+    return {'titles': titles}
+
 
 @register.inclusion_tag('_inc/crm_sections.html')
 def show_content_crm():
     content = CrmContent.objects.filter(is_published=True)
-    return {'content':content}
+    return {'content': content}
+
 
 @register.inclusion_tag('_inc/crm_video.html')
 def show_videos():
     videos = CrmContent.objects.values('video').filter(is_published=True)
-    return {'videos':videos}
+    return {'videos': videos}
+
+
+@register.inclusion_tag('_inc/gallery_title.html')
+def show_gallery_title():
+    gallery_title = GalleriesTitles.objects.filter(is_published=True)
+    return {'gallery_title': gallery_title}
+
+
+@register.inclusion_tag('_inc/gallery_img.html')
+def show_gallery_img():
+    gallery_img = GalleryImages.objects.filter(is_published=True)[:1]
+    gallery_img_1 = GalleryImages.objects.filter(is_published=True)[1:]
+    return {'gallery_img': gallery_img,
+            'gallery_img_1': gallery_img_1
+            }
