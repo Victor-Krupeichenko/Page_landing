ec9affccee91b4e189283154f6cf06cdee561da0
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-19 14:35:52 +0300

    Добавлены пользовательские теги для секции crm

--- landing/templatetags/landing_tags.py
+++ landing/templatetags/landing_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from landing.models import InspireHeader, Inspires
+from landing.models import InspireHeader, Inspires, Crm, CrmContent
 
 register = template.Library()
 
@@ -21,3 +21,18 @@ def show_content():
 def show_design():
     design = Inspires.objects.values('attainment_title', 'attainment_content').filter(is_published=True)
     return {'design':design}
+
+@register.inclusion_tag('_inc/crm_title.html')
+def show_title_crm():
+    titles = Crm.objects.filter(is_published=True)
+    return {'titles':titles}
+
+@register.inclusion_tag('_inc/crm_sections.html')
+def show_content_crm():
+    content = CrmContent.objects.filter(is_published=True)
+    return {'content':content}
+
+@register.inclusion_tag('_inc/crm_video.html')
+def show_videos():
+    videos = CrmContent.objects.values('video').filter(is_published=True)
+    return {'videos':videos}
