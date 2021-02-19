c9a46a5536f09cce97fa341c1474165291af9be6
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-19 14:58:35 +0300

    Добавлен маршрут для просмотра конкретной записи из секции CRM

--- landing/urls.py
+++ landing/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', Index.as_view(), name='home'),
     path('read-more-single/<str:slug>/', InspireViews.as_view(), name='single_inspire'),
     path('single-lets/<str:slug>/', LetsViews.as_view(), name='single_lets'),
+    path('crm-single<str:slug>/', CrmViews.as_view(), name='crm_single'),
     path('reviews-add/', ReviewsCreate.as_view(), name='reviews_add'),
     path('reviews-all/', ReviewsAll.as_view(), name='reviews_all')
 ]
