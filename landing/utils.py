21c4fe529b0d3879b3ba973f6f6fb4588d73e6e0
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-21 16:33:45 +0300

    переопределения метода post для для вывода сообщения при отправки данных из формы

--- landing/utils.py
+++ landing/utils.py
@@ -1,14 +1,26 @@
+from django.contrib import messages
+from django.shortcuts import redirect
 from .forms import FormQuestionsViews
-from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 from .models import ReviewsAddText
 
-class MyFormLanding(CreateView):
+
+class MyMixinFormLanding(CreateView):
     form_class = FormQuestionsViews
     template_name = 'index.html'
-    success_url = reverse_lazy('home')
 
-class MyFormReviewsAdd(ListView):
+    def post(self, request, *args, **kwargs):
+        form = FormQuestionsViews(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"{form.cleaned_data['name']}, Ваш вопрос получен!")
+            return redirect('home')
+        else:
+            messages.error(request, 'Ошибка получения')
+            return redirect('home')
+
+
+class MyMixinFormReviewsAdd(ListView):
     model = ReviewsAddText
     template_name = 'reviews_add.html'
 
