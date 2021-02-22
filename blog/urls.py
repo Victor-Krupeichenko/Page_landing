b9a2c3f07c0b85edd69bd60e13ee227afbad4baf
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-22 19:24:04 +0300

    Добавлен маршрут для просмотра записей по тегу

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NotesViews, NotesByCategories, ViewsNotes
+from .views import *
 
 urlpatterns = [
     path('', NotesViews.as_view(), name='blog'),
     path('category/<str:slug>/', NotesByCategories.as_view(), name='category'),
     path('vievs-notes/<str:slug>/', ViewsNotes.as_view(), name='views_notes'),
+    path('tags-notes/<str:slug>/', NotesTags.as_view(), name='tags_notes')
 ]
