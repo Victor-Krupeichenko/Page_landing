f26a1e17a46be779d83e94baf238654ba6089d33
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-22 19:22:01 +0300

    Добавлена модель тегов, в каждую модель добавлен мето-класс, добален параметр в связи моделей указывающий имя обратного отношения. В модель Тегов добавлен метод get_absolute_url

--- blog/models.py
+++ blog/models.py
@@ -5,19 +5,26 @@ from django.urls import reverse_lazy
 class Notes(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
     slug = models.SlugField(max_length=150, unique=True, db_index=True, verbose_name='URLs')
-    content = models.TextField(verbose_name='Текст статьи')
+    content = models.TextField(verbose_name='Текст статьи', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     update_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     image = models.ImageField(upload_to='image/notes/%m/%d/', verbose_name='Изображение', blank=True)
     views = models.IntegerField(default=0, verbose_name='Просмотры')
     is_published = models.BooleanField(default=False)
-    category = models.ForeignKey('BlogCategories', on_delete=models.PROTECT, verbose_name='Категория')
+    category = models.ForeignKey('BlogCategories', on_delete=models.PROTECT, verbose_name='Категория',
+                                 related_name='category')
+    tag = models.ManyToManyField('Tags', blank=True, related_name='tags', verbose_name='Тег')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse_lazy('views_notes', kwargs={'slug':self.slug})
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+        ordering = ['-created_at']
 
 
 class BlogCategories(models.Model):
@@ -29,3 +36,24 @@ class BlogCategories(models.Model):
 
     def get_absolute_url(self):
         return reverse_lazy('category', kwargs={'slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+
+class Tags(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Тег')
+    slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name='URLs')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['title']
+
+    def get_absolute_url(self):
+        return reverse_lazy('tags_notes', kwargs={'slug':self.slug})
