0293759968e6f8afbd20b8da4a31bb5d278863ee
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-24 17:20:30 +0300

    в пользовательской тег добавлен параметр с момощью которого будет выделятся выбранный пункт меню

--- blog/templatetags/blogs_tags.py
+++ blog/templatetags/blogs_tags.py
@@ -5,8 +5,8 @@ from blog.models import BlogCategories
 register = template.Library()
 
 
-@register.inclusion_tag('sidebar_categories.html')
-def views_categories():
+@register.inclusion_tag('category.html')
+def views_categories(category_select=0):
     categories = BlogCategories.objects.annotate(cnt=Count('category', filter=F('category__is_published'))).filter(
         cnt__gt=0)
-    return {'categories':categories}
+    return {'categories': categories, 'category_select': category_select}
