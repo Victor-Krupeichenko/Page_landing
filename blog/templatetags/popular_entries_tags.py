a82c40baa5cd3e387a9e38830ce63bfb805fbb28
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-25 16:41:38 +0300

    пользовательский тег для облака тегов

--- blog/templatetags/popular_entries_tags.py
+++ blog/templatetags/popular_entries_tags.py
@@ -1,9 +1,16 @@
 from django import template
-from blog.models import Notes
+from blog.models import Notes, Tags
 
 register = template.Library()
+
 
 @register.inclusion_tag('sidebar.html')
 def popular_notes(cnt=3):
     notes = Notes.objects.order_by('-views')[:cnt]
-    return {'notes':notes}
+    return {'notes': notes}
+
+
+@register.inclusion_tag('blog_tags.html')
+def tags_views():
+    tags = Tags.objects.all()
+    return {'tags': tags}
