9d6b515a8a0b9a7c5a7b20585904803ab293babc
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-02-28 18:52:45 +0300

    маршрут для контроллер-класса поиска

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', NotesViews.as_view(), name='blog'),
     path('category/<str:slug>/', NotesByCategories.as_view(), name='category'),
     path('vievs-notes/<str:slug>/', ViewsNotes.as_view(), name='views_notes'),
-    path('tags-notes/<str:slug>/', NotesTags.as_view(), name='tags_notes')
+    path('tags-notes/<str:slug>/', NotesTags.as_view(), name='tags_notes'),
+    path('search/', Search.as_view(), name='search'),
 ]
