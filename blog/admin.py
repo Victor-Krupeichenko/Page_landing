c2c8838e3cafee2a54630e3642b3c5bc5d023c8f
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-03-06 03:28:55 +0300

    регистрация добели комментариев

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.template.defaultfilters import truncatechars
+
 from .models import *
 
 
@@ -14,7 +16,19 @@ class BlogCategoriesAdmin(admin.ModelAdmin):
     list_display = ['title']
     prepopulated_fields = {'slug': ('title',)}
 
+
 @admin.register(Tags)
 class TagsAdmin(admin.ModelAdmin):
     list_display = ['title']
-    prepopulated_fields = {'slug':('title',)}
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(CommentNotes)
+class CommentNotesAdmin(admin.ModelAdmin):
+    list_display = ['name', 'short_text', 'created_at', 'is_published']
+    list_editable = ['is_published']
+
+    def short_text(self, obj):
+        return truncatechars(obj.text, 20)
+
+    short_text.short_description = 'сообщение'
