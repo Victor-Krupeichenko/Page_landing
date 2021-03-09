3ac356e428ffc1fef788fcd53ac60aa45002aaff
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-03-09 03:48:53 +0300

    добавлен маршрут для функции удаления комментария

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('tags-notes/<str:slug>/', NotesTags.as_view(), name='tags_notes'),
     path('search/', Search.as_view(), name='search'),
     path('commit-notes/<int:pk>/', AddComment.as_view(), name='commit_notes'),
+    path('delete-messages/<int:pk>/', delete_messages, name='messages_delete'),
 ]
