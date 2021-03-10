3ca2840b7d8e10b60dfefc76db2a0717e63c5daf
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-03-10 03:36:34 +0300

    Форма для добовления записи: добавлено переопределение атрибута empty_label для изменения 9'-' переопределение атрибута label (заменено Тег на Теги)

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,25 @@
 from django import forms
-from .models import CommentNotes
+from .models import CommentNotes, Notes
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = CommentNotes
         fields = ['name', 'email', 'text']
+
+
+class NotesAddForm(forms.ModelForm):
+    class Meta:
+        model = Notes
+        fields = ['title', 'content', 'image', 'category', 'tag', 'is_published']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'tag': forms.SelectMultiple(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].empty_label = '--- Выберите категорию ---'
+        self.fields['tag'].label = 'Теги'
