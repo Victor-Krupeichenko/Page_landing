42f0a7c17d415af21f605ffdf38fff792f9b8568
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-03-13 20:30:15 +0300

    добавлен widget для изменения внешнего вида отображения формы

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms import Textarea
+
 from .models import CommentNotes, Notes
 
 
@@ -6,6 +8,12 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = CommentNotes
         fields = ['name', 'email', 'text']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].widget = Textarea(attrs={'rows': 4})
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
 
 
 class NotesAddForm(forms.ModelForm):
