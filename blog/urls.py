c58ffcbfc6ecea0cc605aaaae671ecedaddae59b
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2021-03-14 02:28:42 +0300

    маршрут для обновления записи и комментария к записи, изменено имя маршрута для показа текущей записи

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,13 @@ from .views import *
 urlpatterns = [
     path('', NotesViews.as_view(), name='blog'),
     path('category/<str:slug>/', NotesByCategories.as_view(), name='category'),
-    path('vievs-notes/<str:slug>/', ViewsNotes.as_view(), name='views_notes'),
+    path('detail-notes/<str:slug>/', DetailNote.as_view(), name='detail_notes'),
     path('tags-notes/<str:slug>/', NotesTags.as_view(), name='tags_notes'),
     path('search/', Search.as_view(), name='search'),
     path('commit-notes/<int:pk>/', AddComment.as_view(), name='commit_notes'),
     path('delete-messages/<int:pk>/', delete_messages, name='messages_delete'),
     path('notes/create-notes/', CreatedNotes.as_view(), name='created_notes'),
     path('notes-delete/<slug:slug>/', delete_notes, name='delete_notes'),
-    path('update-notes/<str:slug>/', NotesUpdate.as_view(), name='update_notes')
+    path('update-notes/<str:slug>/', NotesUpdate.as_view(), name='update_notes'),
+    path('update_comment/<int:pk>/', CommentsUpdate.as_view(), name='comment_update'),
 ]
